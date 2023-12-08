c797197571403eb3c7c8972c156985325f4024ad
LuidiPires <luidihot@gmail.com>
2023-12-08 14:31:39 -0300

    api port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,5 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3001;
+const PORT: number = Number(process.env.API_PORT);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
